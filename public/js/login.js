c34a5a232de85ce2be01aebd9bac400cf526a3f8
El Roble <san_lucas2005@yahoo.com>
2021-06-23 18:19:41 -0600

    Add changes for form

--- public/js/login.js
+++ public/js/login.js
@@ -25,11 +25,13 @@ const signupFormHandler = async (event) => {
   const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector('#name-signup').value.trim();
+  const lastname = document.querySelector('#lastname-signup').value.trim();
 
   if (username && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ username, email, password, name, lastname }),
       headers: { 'Content-Type': 'application/json' },
     });
 
